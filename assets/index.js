39b061b3b1be5c2907c6a48c34df6c8bcdae875a
[mironmari] <[mironrnko.mv@mail.ru>
2023-06-11 15:38:17 +0100

    first commit

---
+++ assets/index.js
@@ -0,0 +1,126 @@
+
+let objects = [
+  {
+    name: 'Стивен Спилберг',
+    career: 'Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр',
+    films: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F_%D0%A1%D1%82%D0%B8%D0%B2%D0%B5%D0%BD%D0%B0_%D0%A1%D0%BF%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D0%B3%D0%B0',
+    top_rated_film: 'Список Шиндлера'
+  },
+  {
+    name: 'Кристофер Нолан',
+    career: 'Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор',
+    films: 'https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu',
+    top_rated_film: 'Начало'
+  },
+  {
+    name: 'Мартин МакДона',
+    career: 'Сценарист, Режиссёр, Продюсер',
+    films: 'https://www.film.ru/person/martin-makdonah',
+    top_rated_film: 'Три билборда на границе Эббинга, Миссури'
+  },
+  {
+    name: 'Алексей Балабанов',
+    career: 'Режиссёр, Сценарист, Актёр, Продюсер',
+    films: 'https://www.film.ru/person/aleksey-balabanov',
+    top_rated_film: 'Брат'
+  },
+  {
+    name: 'Питер Фаррелли',
+    career: 'Продюсер, Режиссёр, Сценарист, Актёр',
+    films: 'https://www.film.ru/person/piter-farrelli',
+    top_rated_film: 'Зелёная книга'
+  },
+  {
+    name: 'Юрий Быков',
+    career: 'Актёр, Режиссёр, Сценарист, Композитор, Монтажёр, Продюсер',
+    films: 'https://www.film.ru/person/yuriy-bykov',
+    top_rated_film: 'Дурак'
+  },
+  {
+    name: 'Жан-Марк Валле',
+    career: 'Режиссер, Продюсер, Монтажёр, Актёр, Сценарист',
+    films: 'https://www.film.ru/person/zhan-mark-valle',
+    top_rated_film: 'Далласский клуб покупателей'
+  },
+];
+
+
+/*const button = document.createElement('button')
+button.innerText = 'Click me';
+document.body.appendChild(button);
+
+button.addEventListener('click', () => {
+  arr.forEach(function (item) {
+    console.log(item + '.');
+});
+})*/
+/*const box = document.createElement("div");
+box.innerText = '1';
+box.classList.add("box");
+const container = document.getElementById("directors");
+container.append(box);
+
+
+const name1 = document.createElement("p");
+name1.innerText = `Name: `;
+box.append(name1);*/
+
+document.write('<ol start="1">');
+objects.forEach( object => {
+document.write(`<li><span style="font-size: 24px">${object.name}</span><br>
+<span style="color: #58C8E9">career: ${object.career}</span><br>
+<span style="color: #F45B5D">films: ${object.films}<br>`);
+});
+document.write('<ol>');
+
+let bestFilms = [
+  {
+    name: 'Стивен Спилберг',
+    career: 'Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр',
+    films: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F_%D0%A1%D1%82%D0%B8%D0%B2%D0%B5%D0%BD%D0%B0_%D0%A1%D0%BF%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D0%B3%D0%B0',
+    top_rated_film: 'Список Шиндлера'
+  },
+  {
+    name: 'Кристофер Нолан',
+    career: 'Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор',
+    films: 'https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu',
+    top_rated_film: 'Начало'
+  },
+  {
+    name: 'Мартин МакДона',
+    career: 'Сценарист, Режиссёр, Продюсер',
+    films: 'https://www.film.ru/person/martin-makdonah',
+    top_rated_film: 'Три билборда на границе Эббинга, Миссури'
+  },
+  {
+    name: 'Алексей Балабанов',
+    career: 'Режиссёр, Сценарист, Актёр, Продюсер',
+    films: 'https://www.film.ru/person/aleksey-balabanov',
+    top_rated_film: 'Брат'
+  },
+  {
+    name: 'Питер Фаррелли',
+    career: 'Продюсер, Режиссёр, Сценарист, Актёр',
+    films: 'https://www.film.ru/person/piter-farrelli',
+    top_rated_film: 'Зелёная книга'
+  },
+  {
+    name: 'Юрий Быков',
+    career: 'Актёр, Режиссёр, Сценарист, Композитор, Монтажёр, Продюсер',
+    films: 'https://www.film.ru/person/yuriy-bykov',
+    top_rated_film: 'Дурак'
+  },
+  {
+    name: 'Жан-Марк Валле',
+    career: 'Режиссер, Продюсер, Монтажёр, Актёр, Сценарист',
+    films: 'https://www.film.ru/person/zhan-mark-valle',
+    top_rated_film: 'Далласский клуб покупателей'
+  },
+];
+
+document.write('<span style="color: #000000"><h2>Лучшие фильмы этиих режиссёров</h2>');
+
+bestFilms.forEach( bestFilms => {
+document.write(`<li><span style="font-size: 20px">${bestFilms.top_rated_film}</span><br>
+`);
+});
